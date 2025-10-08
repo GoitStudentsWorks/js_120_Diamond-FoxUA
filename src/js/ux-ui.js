7157d1f94bb1225a214c123d069d550f1419163c
Dmytro Farbun <dimofarb@gmail.com>
2025-10-08 20:48:01 +0300

    ux-ui.js – add iziToast

---
+++ src/js/ux-ui.js
@@ -0,0 +1,23 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+export function showError(message) {
+  iziToast.error({
+    message,
+    position: 'topRight',
+  })
+}
+
+export function showInfo(message) {
+  iziToast.info({
+    message,
+    position: 'topRight'
+  })
+}
+
+export function showWarning(message) {
+  iziToast.warning({
+    message,
+    position: 'topRight'
+  })
+}
